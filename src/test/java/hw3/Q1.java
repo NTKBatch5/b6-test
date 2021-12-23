6570a039603aa8d56d76c569af44c2e917ae53c7
aghaz <aghaz@LAPTOP-L2PTAS43.myfiosgateway.com>
2021-12-22 20:31:09 -0500

    Q1 and Q2...

---
+++ src/test/java/hw3/Q1.java
@@ -0,0 +1,9 @@
+package hw3;
+
+public class Q1 {
+	
+	private void name() {
+		System.out.println("Q1");
+	}
+
+}
---
+++ src/test/java/hw3/Q2.java
@@ -0,0 +1,8 @@
+package hw3;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+		System.out.println("Q2");
+	}
+}
